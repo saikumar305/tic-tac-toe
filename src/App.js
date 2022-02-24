eb4412665dcfbfd54bb737aa2f8cb4961230c07c
saikumar305 <saikumarrachakonda305@gmail.com>
2022-02-25 00:59:38 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+import TicTacToe from "./components/TicTacToe";
+import "papercss/dist/paper.min.css";
+
+function App() {
+  return (
+    <>
+      <Main>
+        <TicTacToe />
+      </Main>
+      <Footer>
+        <FooterInner>Made by T_T</FooterInner>
+      </Footer>
+    </>
+  );
+}
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 1 0 auto;
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  flex: 0 0 auto;
+`;
+
+const FooterInner = styled.div`
+  padding: 16px 0;
+`;
+export default App;
